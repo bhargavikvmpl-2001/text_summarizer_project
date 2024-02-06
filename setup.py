a35a2f742df8c3c8be4daa3011ed0319f37b58d6
Bhargavi <71485506+bhargavikvmpl-2001@users.noreply.github.com>
2024-02-06 17:02:10 +0530

    reuirements installed

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding ="utf-8") as f:
+    long_description = f.read()
+    
+__version__ = "0.0.0"
+
+REPO_NAME = "text_summarizer_project"
+AUTHOR_USER_NAME = "bhargavikvmpl-2001"
+SRC_REPO = "textSummarizer"
+AUTHOR_EMAIL = "kvmplbhargavi@gmail.com"
+
+setuptools.setup(
+    name=REPO_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for NLP app",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/",
+    project_urls={"Bug Tracker": "https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}}",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
